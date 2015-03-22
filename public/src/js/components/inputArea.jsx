1187e1cd7c70846409e636a05446a820edd4ddfa
Raymond Chan <am5a03@gmail.com>
2015-03-22 22:36:48 +0800

    Handling changes purely in react

--- public/src/js/components/inputArea.jsx
+++ public/src/js/components/inputArea.jsx
@@ -4,10 +4,28 @@ var InputArea = React.createClass({
 			value: ''
 		};
 	},
+	onKeyDown: function(e) {
+		if (e.keyCode == 13) {
+			var currentExpression = e.currentTarget.value;			
+			try {
+				var answer = eval(currentExpression);
+				this.props.writeHistory({ 'formula' : currentExpression, 'answer' : answer});
+			} catch (ex) {
+
+			} finally {
+				this.setState({value : ''});
+			}
+		}
+	},
+	onChange: function(e) {
+		this.setState({
+			value : e.target.value
+		});
+	},
 	render: function () {
 		return (
 			<div className="input-area">
-				<input className="input-area__input" placeholder="Expression"/>
+				<input className="input-area__input" placeholder="Expression" onChange={ this.onChange } onKeyDown={ this.onKeyDown } value={this.state.value} />
 			</div>
 		);
 	}
--- public/src/js/partials/historyBox.jsx
+++ public/src/js/partials/historyBox.jsx
@@ -1,9 +1,34 @@
 var InputArea = require('../components/inputArea');
 var HistoryBox = React.createClass({
+	getInitialState: function() {
+		var histories = {
+			'histories' : [
+
+			]
+		}
+		return histories;
+	},
+	onInputAreaChange: function(e) {
+		console.log(e);
+	},
+	onWriteHistory: function(history) {
+		console.log(history);
+	},
 	render: function() {
+		var histories = this.state.histories.map(function (history, index) {
+			return (
+				<li key={ history.id } className="historic-formula">
+					{ history.formula }
+				</li>
+			);
+		});
+
 		return (
 			<div className="history-box">
-				<InputArea />
+				<ul className="history-box__list">
+					{ histories }
+				</ul>
+				<InputArea writeHistory={this.onWriteHistory} />
 			</div>
 		);
 	}
